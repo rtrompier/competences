e2e1298e28d0b3f77382163398519e4ccb8dc53e
wendannor <jordan.sarre@gmail.com>
2014-03-18 13:08:20 +0100

    First commit
    osef

---
+++ app.js
@@ -0,0 +1,33 @@
+/**
+ * Created by asgard on 18/03/14.
+ */
+'use strict;'
+var app = angular.module('CompetenceApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        })
+        .otherwise({
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        });
+});
+
+app.controller('HomeCtrl', function ($scope, $http) {
+    var competences = ['lol', 'lil', 'lal'];
+    $scope.competences = competences;
+    $http.get('http://192.168.0.93:3000/competences')
+        .success(function (data) {
+            $scope.competences = data;
+        })
+        .error(function () {
+
+        });
+
+    $http.get('foo.json').then(function(result) {
+        return result.data;
+    });
+});
